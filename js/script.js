cdc057ddc14e91ad5d5a52bbb546b7feb4a14eab
Danilo <danilo.colloca91@gmail.com>
2021-02-09 19:18:25 +0100

    header color/position

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 new Vue ({
-    el : '#app',
-    data : {
-        userSearch: '',
-        searchList : []
+  el : '#app',
+  data : {
+    userSearch: '',
+    searchList : []
 
-    },
-    methods : {
-        //ricerca dinamica del titolo
-        isSearchFilm : function(){
-          const searchkey = this.userSearch;
-          let self = this;
-          axios
-            .get('https://api.themoviedb.org/3/search/movie?api_key=d6a99b8f732b4dd111faf2e38c0dc146&query=' + searchkey)
-            .then(function(returned){
-                self.searchList = returned.data.results
-                console.log(self.searchList)
-            })
-        }
+  },
+  methods : {
+    //ricerca dinamica del titolo
+    isSearchFilm : function(){
+      const searchkey = this.userSearch;
+      let self = this;
+      axios
+      .get('https://api.themoviedb.org/3/search/movie?api_key=d6a99b8f732b4dd111faf2e38c0dc146&query=' + searchkey)
+      .then(function(returned){
+        self.searchList = returned.data.results
+        console.log(self.searchList)
+      })
     }
+  }
 
 })
 Vue.config.devtools = true;
