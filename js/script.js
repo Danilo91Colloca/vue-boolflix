d5755fd9a15244061daee3fbd5142ef375783ea2
Danilo <danilo.colloca91@gmail.com>
2021-02-10 18:42:17 +0100

    implementazione ricerca serie tv

--- js/script.js
+++ js/script.js
@@ -6,18 +6,30 @@ new Vue ({
     langList : ['de', 'en', 'fr', 'it', 'pt', 'us', 'zh']
   },
   methods : {
-    isSearchFilm : function(){                    /*ricerca dinamica del titolo*/
-      let self = this;
-      axios
-      .get('https://api.themoviedb.org/3/search/movie', {
+    isSearchFilm : function(){             /*ricerca dinamica del titolo*/
+      if(this.userSearch !== '') {
+        axios
+        .get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+            query : this.userSearch 
+          }
+        })
+        .then((returned)=>{
+          this.searchList = returned.data.results      
+        });
+  
+        axios
+        .get('https://api.themoviedb.org/3/search/tv', {
           params: {
-          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-          query : this.userSearch 
-        }
-      })
-      .then((returned)=>{
-        this.searchList = returned.data.results      
-      })
+            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+            query : this.userSearch 
+          }
+        })
+        .then((returned)=>{
+          this.searchList = returned.data.results      
+        });
+      }    
     },      
     starsVote : function(nVote){
       return parseInt(nVote / 2)
