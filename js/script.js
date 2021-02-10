467f2cf6a634c929a472f6c50e0dff65eee94301
Danilo <danilo.colloca91@gmail.com>
2021-02-10 17:05:23 +0100

    correzione sovrascrittura dato/ e stelle funzionanti

--- js/script.js
+++ js/script.js
@@ -3,44 +3,24 @@ new Vue ({
   data : {
     userSearch: '',
     searchList : [],
-    star: 'far',
-
   },
   methods : {
     isSearchFilm : function(){                    /*ricerca dinamica del titolo*/
-      const searchkey = this.userSearch;
       let self = this;
       axios
-      .get('https://api.themoviedb.org/3/search/movie?api_key=d6a99b8f732b4dd111faf2e38c0dc146&query=' + searchkey)
-      .then(function(returned){
-        self.searchList = returned.data.results
-        console.log(self.searchList);
-        self.searchList.map(element => {          /*estraggo solo il voto e lancio funzione isIntVote*/
-          element.vote_average = self.isIntVote(element.vote_average)
-        });
-        self.searchList.forEach((element, index) => {  
-          console.log(element.vote_average, index) 
-        });
-
-
+      .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.userSearch 
+        }
       })
-    },
-    isIntVote : function(vote){            /*trasformo il voto in intero compreso tra 0 e 5*/ 
-      let intNum; 
-      if(vote === 1){
-        intNum = 1
-      } else {
-        intNum = parseInt(vote / 2)
-      }
-      return intNum
-      // console.log(intNum)      
-    },
-    voteInList : function(nVote){
-
-    }
-    
-   
-    
+      .then((returned)=>{
+        this.searchList = returned.data.results      
+      })
+    },      
+    starsVote : function(nVote){
+      return parseInt(nVote / 2)
+    }  
     
   }
 })
