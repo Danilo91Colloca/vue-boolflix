6a23ab111a662d333f09a8a6f88db029b750a11a
Danilo <danilo.colloca91@gmail.com>
2021-02-10 09:42:09 +0100

    funzione che trasforma i numeri in interi da 1 a 5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ new Vue ({
   el : '#app',
   data : {
     userSearch: '',
-    searchList : []
+    searchList : [],
 
   },
   methods : {
@@ -16,8 +16,22 @@ new Vue ({
         self.searchList = returned.data.results
         console.log(self.searchList)
       })
+    },
+    isIntVote : function(vote){
+      let intNum; 
+      if(vote === 1){
+        intNum = 1
+      } else {
+        intNum = parseInt(vote / 2)
+      }
+      return intNum
+
     }
   }
 
 })
 Vue.config.devtools = true;
+
+/*
+https://api.themoviedb.org/3/search/tv?api_key=d6a99b8f732b4dd111faf2e38c0dc146&language=it_IT&query=
+*/
