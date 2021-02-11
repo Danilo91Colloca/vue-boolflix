5ca54b399c4fffdb0b29ead752eb6aacfc119cba
Danilo <danilo.colloca91@gmail.com>
2021-02-11 09:18:06 +0100

    pulizia html e commenti
    aggiunta funzione per gestire i parametri provenienti da film o tv che hanno key diverse per rappresentare lo stesso valore

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ new Vue ({
   methods : {
     isSearchFilm : function(){             /*ricerca dinamica del titolo*/
       if(this.userSearch !== '') {
-        axios
+        axios /* chiamata xhr per la sezione FILM */
         .get('https://api.themoviedb.org/3/search/movie', {
             params: {
             api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
@@ -20,7 +20,7 @@ new Vue ({
           this.searchList = returned.data.results      
         });
   
-        axios
+        axios /*chiamata xhr per la sezione TV*/ 
         .get('https://api.themoviedb.org/3/search/tv', {
           params: {
             api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
@@ -32,8 +32,14 @@ new Vue ({
         });
       }    
     },      
-    starsVote : function(nVote){
+    starsVote : function(nVote){ /*ritorna il voto medio in intero e alla sua metà*/
       return parseInt(nVote / 2)
+    },
+    isFilmTvParam : function(title, name){ /*ritorna parametri diversi se la query è film o tv*/
+      if(title){
+        return title
+      }    
+      return name
     }  
     
   }
