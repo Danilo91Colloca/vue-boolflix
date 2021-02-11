71fe67083d500888675eb21fde3243b70f6bee39
Danilo <danilo.colloca91@gmail.com>
2021-02-11 11:59:42 +0100

    refactoring: separazione delle ricerche

--- js/script.js
+++ js/script.js
@@ -2,11 +2,18 @@ new Vue ({
   el : '#app',
   data : {
     userSearch: '',
-    searchList : [],
+    allSearchList : [],
+    movieList : [],
+    tvList : [],
     langList : ['de', 'en', 'fr', 'it', 'pt', 'us', 'zh']
   },
   methods : {
-    isSearchFilm : function(){     /*ricerca dinamica del titolo e ritorna sia tv che films*/
+    search : function(){
+      this.allSearchList = [];
+      this.searchFilm();
+      this.searchTv();
+    },
+    searchFilm : function(){     /*ricerca dinamica del titolo e ritorna films*/
       if(this.userSearch !== '') {
         axios /* chiamata xhr per la sezione FILM */
         .get('https://api.themoviedb.org/3/search/movie', {
@@ -17,22 +24,24 @@ new Vue ({
           }
         })
         .then((returned)=>{
-          let film = returned.data.results  
-          axios /*chiamata xhr per la sezione TV*/ 
-          .get('https://api.themoviedb.org/3/search/tv', {
-            params: {
-              api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-              query : this.userSearch 
-            }
-          })
-          .then((returned)=>{
-            let tv = returned.data.results      
-            this.searchList = [...film, ...tv]
-          });    
-        });
-  
-        
+          this.movieList = returned.data.results;
+          this.allSearchList = [...this.allSearchList, ...this.movieList]  
+        });        
       }    
+    },
+    searchTv: function() {
+      axios /*chiamata xhr per la sezione TV*/ 
+        .get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+            query : this.userSearch 
+          }
+        })
+        .then((returned)=>{
+          this.tvList = returned.data.results;      
+          this.allSearchList = [...this.allSearchList, ...this.tvList]
+        }); 
+
     },      
     starsVote : function(nVote){ /*ritorna il voto medio in intero e alla sua metà*/
       return parseInt(nVote / 2)
