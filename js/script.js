dc9f6cd138360bc90d80470662bd0e0ca54a406a
Danilo <danilo.colloca91@gmail.com>
2021-02-12 10:03:27 +0100

    miglioramento ricerca per genere / no by select

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ new Vue ({
       .get('https://api.themoviedb.org/3/search/movie', {
           params: {
           api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-          query : 'a ',
+          query : this.randomLetter(), //query random
           language : 'it_IT' 
         }
       })
@@ -41,7 +41,7 @@ new Vue ({
       .get('https://api.themoviedb.org/3/search/tv', {
         params: {
           api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-          query : 's' 
+          query : this.randomLetter() //query random 
         }
       })
       .then((returned)=>{
@@ -51,73 +51,69 @@ new Vue ({
     },
     search : function(){
       this.allSearchList = []; //azzera lista ricerca
-      this.searchFilm(); //richiesta movie
-      this.searchTv(); //richiesta tv
+      // this.searchFilm(); //richiesta movie
+      // this.searchTv(); //richiesta tv
       this.searchPerson(); //richiesta persone
+      
+      // this.searchGenMovId();
+      // this.movieByGenre();
 
-      this.searchGenMovId();
-      this.movieByGenre();
-
-      this.searchGenTvId();
-      this.tvByGenre();
+      // this.searchGenTvId();
+      // this.tvByGenre();
 
       if(this.userSearch ===''){
         this.defaultView();
       }
     },
     searchFilm : function(){     /*ricerca dinamica del titolo e ritorna films*/
-      if(this.userSearch !== '') {
-        axios /* chiamata xhr per la sezione FILM */
-        .get('https://api.themoviedb.org/3/search/movie', {
-            params: {
-            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-            query : this.userSearch,
-            language : 'it_IT' 
-          }
-        })
-        .then((returned)=>{
-          this.movieList = returned.data.results;
-          this.allSearchList = [...this.allSearchList, ...this.movieList]  
-        });        
-      }  
+      axios /* chiamata xhr per la sezione FILM */
+      .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.userSearch,
+          language : 'it_IT' 
+        }
+      })
+      .then((returned)=>{
+        this.movieList = returned.data.results;
+        this.allSearchList = [...this.allSearchList, ...this.movieList]  
+      });        
     },
     searchTv: function() {
-      if(this.userSearch !== ''){
-        axios /*chiamata xhr per la sezione TV*/ 
-        .get('https://api.themoviedb.org/3/search/tv', {
-          params: {
-            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-            query : this.userSearch 
-          }
-        })
-        .then((returned)=>{
-          this.tvList = returned.data.results;      
-          this.allSearchList = [...this.allSearchList, ...this.tvList]
-        }); 
-      }
+      axios /*chiamata xhr per la sezione TV*/ 
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.userSearch 
+        }
+      })
+      .then((returned)=>{
+        this.tvList = returned.data.results;      
+        this.allSearchList = [...this.allSearchList, ...this.tvList]
+      }); 
+
     },
     searchPerson: function(){ /*ricerca tramite nome di attore/regista*/ 
-      if(this.userSearch !== '') {
-        axios /* chiamata xhr per person */
-        .get('https://api.themoviedb.org/3/search/person', {
-            params: {
-            api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
-            query : this.userSearch,
-            language : 'it_IT' 
-          }
-        })
-        .then((returned)=>{
-          let person = returned.data.results;
-          this.filterByPerson(person)
-        });        
-      } 
+      axios /* chiamata xhr per person */
+      .get('https://api.themoviedb.org/3/search/person', {
+          params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.userSearch,
+          language : 'it_IT' 
+        }
+      })
+      .then((returned)=>{
+        let person = returned.data.results;
+        this.filterByPerson(person)
+      });        
+
     }, 
     filterByPerson : function(arrayObj){ /*filtra la ricerca persona e implementa con dati movie*/
       arrayObj.forEach(element => {
         this.allSearchList = [...this.allSearchList, ...element.known_for]
       });
     }, 
-    getMovieGenre: function(){ /* crea array generi film tramite genere */
+    getMovieGenre: function(){ /* crea lista di tutti i generi movie disponibili */
         axios
         .get('https://api.themoviedb.org/3/genre/movie/list', {
           params: {
@@ -131,7 +127,7 @@ new Vue ({
 
 
     },
-    getTvGenre: function(){ /* crea array generi tv tramite genere */
+    getTvGenre: function(){  /* crea lista di tutti i generi tv disponibili */
         axios
         .get('https://api.themoviedb.org/3/genre/tv/list', {
           params: {
@@ -144,7 +140,7 @@ new Vue ({
         });
 
     },  
-    searchGenMovId: function() { //se la parola inserita è inclusa ritorna il codice id
+    searchGenMovId: function() { //trasforma il nome genere inserito dall'utente in cod id
       let genName= this.capitalize(this.userSearch);
       let id;
       this.movieGenres.forEach((element) =>{
@@ -157,11 +153,24 @@ new Vue ({
       })
     }, 
     movieByGenre: function(){ //filtra movieList inserendo film che includono idCodeMov
-      this.movieList = this.movieList.filter((element)=>{ 
-        return element.genre_ids.includes(this.idCodeMov)               
+      axios /* chiamata xhr per la sezione FILM */
+      .get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.randomLetter(), //query random
+          language : 'it_IT' 
+        }
       })
+      .then((returned)=>{ //ritorna solo i film che contengono idCodeMov
+        this.movieList = returned.data.results;
+        this.movieList = this.movieList.filter((element)=>{ 
+          return element.genre_ids.includes(this.idCodeMov)
+        })
+        this.allSearchList = [...this.allSearchList, ...this.movieList]  
+      }); 
+      
     }, 
-    searchGenTvId: function() { //se la parola inserita è inclusa ritorna il codice id
+    searchGenTvId: function() { //trasforma il nome genere inserito dall'utente in cod id
       let genName= this.capitalize(this.userSearch);
       let id;
       this.tvGenres.forEach((element) =>{
@@ -173,9 +182,20 @@ new Vue ({
       })
     }, 
     tvByGenre: function(){ //filtra tvList inserendo film che includono idCodeTv
-      this.tvList = this.tvList.filter((element)=>{
-        return element.genre_ids.includes(this.idCodeTv)               
+      axios /*chiamata xhr per la sezione TV*/ 
+      .get('https://api.themoviedb.org/3/search/tv', {
+        params: {
+          api_key : 'd6a99b8f732b4dd111faf2e38c0dc146',
+          query : this.randomLetter() //query random 
+        }
       })
+      .then((returned)=>{
+        this.tvList = returned.data.results; 
+        this.tvList = this.tvList.filter((element)=>{
+          return element.genre_ids.includes(this.idCodeTv)               
+        })     
+        this.allSearchList = [...this.allSearchList, ...this.tvList]
+      });     
     },  
     starsVote : function(nVote){ /*ritorna il voto medio in intero e alla sua metà*/
       return parseInt(nVote / 2)
@@ -186,12 +206,20 @@ new Vue ({
       }    
       return name
     },
-    capitalize: function(phrase){ //crea un capitalize
+    capitalize: function(phrase){ //funzione capitalize
       return phrase
         .toLowerCase()
         .split(' ')
         .map(word => word.charAt(0).toUpperCase() + word.slice(1))
         .join(' ');
+    },
+    randomLetter: function(){ //prende 2 lettere random dall'alfabeto
+      let result = '';
+      let alfabeto = 'abcdefghijklmnopqrstuvwxyz';
+      for(let i=0; i<=2; i++){
+        result = alfabeto.charAt(Math.floor(Math.random() * alfabeto.length))
+      }
+      return result
     }
         
   }
